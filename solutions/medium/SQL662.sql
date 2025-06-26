349df1a7b510f9c3278a0de962fa538c49b588e2
Eldar <enalchikov@gmail.com>
2025-06-26 12:13:49 +0300

    Уточнил задачу

--- solutions/medium/SQL662.sql
+++ solutions/medium/SQL662.sql
@@ -6,7 +6,11 @@
 +----------------+---------+
 (requester_id, accepter_id) is the primary key (combination of columns with unique values) for this table.
 This table contains the ID of the user who sent the request, the ID of the user who received 
-the request, and the date when the request was accepted.*/
+the request, and the date when the request was accepted.
+
+Task: Write a solution to find the people who have the most friends and the most friends number.
+The test cases are generated so that only one person has the most friends.
+*/
 
 WITH accepter AS(
     SELECT DISTINCT accepter_id, 
